eca1dc330d13da91af90a362c408ca16219f644a
Dmitry Rybin <d.rybin@office.ngs.ru>
2025-03-31 21:45:01 +0700

    Add more amount variant to check

--- internal/app/cli/usecase/search_web.go
+++ internal/app/cli/usecase/search_web.go
@@ -1,99 +1,99 @@
 package usecase
 
 import (
-	"context"
-	"fmt"
-	"strconv"
-	"strings"
-
-	"github.com/drybin/minter-pools-ar/internal/adapter/webapi"
-	"github.com/drybin/minter-pools-ar/internal/domain/model"
-	"github.com/drybin/minter-pools-ar/pkg/telegram"
-	"github.com/drybin/minter-pools-ar/pkg/wrap"
+    "context"
+    "fmt"
+    "strconv"
+    "strings"
+    
+    "github.com/drybin/minter-pools-ar/internal/adapter/webapi"
+    "github.com/drybin/minter-pools-ar/internal/domain/model"
+    "github.com/drybin/minter-pools-ar/pkg/telegram"
+    "github.com/drybin/minter-pools-ar/pkg/wrap"
 )
 
 type ISearchWeb interface {
-	Process(ctx context.Context) error
+    Process(ctx context.Context) error
 }
 
 type SearchWeb struct {
-	MinterWeb    *webapi.MinterWeb
-	MinterWebapi *webapi.MinterWebapi
-	TgWebapi     *telegram.TelegramWebapi
+    MinterWeb    *webapi.MinterWeb
+    MinterWebapi *webapi.MinterWebapi
+    TgWebapi     *telegram.TelegramWebapi
 }
 
 func NewSearchWebUsecase(
-	minterWeb *webapi.MinterWeb,
-	minterWebapi *webapi.MinterWebapi,
-	tgWebapi *telegram.TelegramWebapi,
+    minterWeb *webapi.MinterWeb,
+    minterWebapi *webapi.MinterWebapi,
+    tgWebapi *telegram.TelegramWebapi,
 ) *SearchWeb {
-	return &SearchWeb{
-		MinterWeb:    minterWeb,
-		MinterWebapi: minterWebapi,
-		TgWebapi:     tgWebapi,
-	}
+    return &SearchWeb{
+        MinterWeb:    minterWeb,
+        MinterWebapi: minterWebapi,
+        TgWebapi:     tgWebapi,
+    }
 }
 
 func (u *SearchWeb) Process(ctx context.Context) error {
-	prices := []int{300, 500, 1000, 3000, 5000, 10000}
-	for _, price := range prices {
-		r, err := u.MinterWeb.GetPrice(ctx, price)
-		if err != nil {
-			return wrap.Errorf("failed to get minter swap info: %w", err)
-		}
-
-		commission, err := u.MinterWeb.GetCommission(ctx, r, price)
-		if err != nil {
-			return wrap.Errorf("failed to get minter swap commission info: %w", err)
-		}
-
-		amountIn, err := strconv.ParseFloat(strings.TrimSpace(r.AmountIn), 64)
-		if err != nil {
-			return wrap.Errorf("failed to parse amountIn as float: %w", err)
-		}
-
-		amountOut, err := strconv.ParseFloat(strings.TrimSpace(r.AmountOut), 64)
-		if err != nil {
-			return wrap.Errorf("failed to parse amountIn as float: %w", err)
-		}
-
-		result := amountIn + *commission
-		if result < amountOut {
-			fmt.Println("SUCCESS")
-			fmt.Printf("Processing price %d\n", price)
-			fmt.Printf("r: %v\n", r)
-			fmt.Printf("result: %f\n", result)
-			fmt.Printf("com: %f\n", *commission)
-			res, err := u.MinterWebapi.BuyRaw(
-				ctx,
-				*r,
-			)
-
-			if err != nil {
-				return wrap.Errorf("failed to process exchange: %w", err)
-			}
-
-			msg := generateTgMessage(*res, *commission)
-			_, err = u.TgWebapi.Send(msg)
-			if err != nil {
-				return wrap.Errorf("failed to send TG message: %w", err)
-			}
-		}
-	}
-
-	fmt.Println("All done")
-	return nil
+    prices := []int{300, 500, 1000, 3000, 5000, 10000, 15000, 20000}
+    for _, price := range prices {
+        r, err := u.MinterWeb.GetPrice(ctx, price)
+        if err != nil {
+            return wrap.Errorf("failed to get minter swap info: %w", err)
+        }
+        
+        commission, err := u.MinterWeb.GetCommission(ctx, r, price)
+        if err != nil {
+            return wrap.Errorf("failed to get minter swap commission info: %w", err)
+        }
+        
+        amountIn, err := strconv.ParseFloat(strings.TrimSpace(r.AmountIn), 64)
+        if err != nil {
+            return wrap.Errorf("failed to parse amountIn as float: %w", err)
+        }
+        
+        amountOut, err := strconv.ParseFloat(strings.TrimSpace(r.AmountOut), 64)
+        if err != nil {
+            return wrap.Errorf("failed to parse amountIn as float: %w", err)
+        }
+        
+        result := amountIn + *commission
+        if result < amountOut {
+            fmt.Println("SUCCESS")
+            fmt.Printf("Processing price %d\n", price)
+            fmt.Printf("r: %v\n", r)
+            fmt.Printf("result: %f\n", result)
+            fmt.Printf("com: %f\n", *commission)
+            res, err := u.MinterWebapi.BuyRaw(
+                ctx,
+                *r,
+            )
+            
+            if err != nil {
+                return wrap.Errorf("failed to process exchange: %w", err)
+            }
+            
+            msg := generateTgMessage(*res, *commission)
+            _, err = u.TgWebapi.Send(msg)
+            if err != nil {
+                return wrap.Errorf("failed to send TG message: %w", err)
+            }
+        }
+    }
+    
+    fmt.Println("All done")
+    return nil
 }
 
 func generateTgMessage(response model.BuyRawResponse, commission float64) string {
-	newLine := "\n"
-
-	return fmt.Sprintf(
-		"Баланс %.2f"+newLine+
-			"amountIn %d amountOut %d commission %.2f"+newLine,
-		response.Balance,
-		response.AmountIn,
-		response.AmountOut,
-		commission,
-	)
+    newLine := "\n"
+    
+    return fmt.Sprintf(
+        "Баланс %.2f"+newLine+
+            "amountIn %d amountOut %d commission %.2f"+newLine,
+        response.Balance,
+        response.AmountIn,
+        response.AmountOut,
+        commission,
+    )
 }
--- internal/domain/helpers/array.go
+++ internal/domain/helpers/array.go
@@ -1,80 +1,80 @@
 package helpers
 
 import (
-    "sort"
-    
-    "github.com/drybin/minter-pools-ar/internal/domain/model"
+	"sort"
+
+	"github.com/drybin/minter-pools-ar/internal/domain/model"
 )
 
 func UniqueStrArray(arr []string) []string {
-    uniqueMap := make(map[string]bool)
-    var uniqueSlice []string
-    
-    for _, num := range arr {
-        if !uniqueMap[num] {
-            uniqueSlice = append(uniqueSlice, num)
-            uniqueMap[num] = true
-        }
-    }
-    
-    return uniqueSlice
+	uniqueMap := make(map[string]bool)
+	var uniqueSlice []string
+
+	for _, num := range arr {
+		if !uniqueMap[num] {
+			uniqueSlice = append(uniqueSlice, num)
+			uniqueMap[num] = true
+		}
+	}
+
+	return uniqueSlice
 }
 
 func SortByLiquidity(items []model.ChainikCoin) {
-    sort.Slice(items, func(i, j int) bool {
-        return items[i].Liquidity > items[j].Liquidity
-    })
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Liquidity > items[j].Liquidity
+	})
 }
 
 func SortPathByProfit(items []model.Path) {
-    sort.Slice(items, func(i, j int) bool {
-        return items[i].Profit > items[j].Profit
-    })
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Profit > items[j].Profit
+	})
 }
 
 func RemovePairFromArray(data []model.ChainikCoin, pairToRemove model.ChainikCoin) []model.ChainikCoin {
-    result := []model.ChainikCoin{}
-    for _, pair := range data {
-        if pair.Id == pairToRemove.Id {
-            continue
-        }
-        result = append(result, pair)
-    }
-    
-    return result
+	result := []model.ChainikCoin{}
+	for _, pair := range data {
+		if pair.Id == pairToRemove.Id {
+			continue
+		}
+		result = append(result, pair)
+	}
+
+	return result
 }
 
 func RemoveLowLiquidityPairFromArray(data []model.ChainikCoin) []model.ChainikCoin {
-    result := []model.ChainikCoin{}
-    for _, pair := range data {
-        if pair.Liquidity < 2 {
-            continue
-        }
-        result = append(result, pair)
-    }
-    
-    return result
+	result := []model.ChainikCoin{}
+	for _, pair := range data {
+		if pair.Liquidity < 2 {
+			continue
+		}
+		result = append(result, pair)
+	}
+
+	return result
 }
 
 func SortArrayByCoinAndLiquidity(data []model.Path) []model.Path {
-    uniqCoin := []string{}
-    result := []model.Path{}
-    
-    for _, item := range data {
-        uniqCoin = append(uniqCoin, item.Coin.Name)
-    }
-    
-    uniqCoin = UniqueStrArray(uniqCoin)
-    for _, coin := range uniqCoin {
-        allPathWithCoin := []model.Path{}
-        for _, item := range data {
-            if item.Coin.Name == coin {
-                allPathWithCoin = append(allPathWithCoin, item)
-            }
-        }
-        SortPathByProfit(allPathWithCoin)
-        result = append(result, allPathWithCoin...)
-    }
-    
-    return result
+	uniqCoin := []string{}
+	result := []model.Path{}
+
+	for _, item := range data {
+		uniqCoin = append(uniqCoin, item.Coin.Name)
+	}
+
+	uniqCoin = UniqueStrArray(uniqCoin)
+	for _, coin := range uniqCoin {
+		allPathWithCoin := []model.Path{}
+		for _, item := range data {
+			if item.Coin.Name == coin {
+				allPathWithCoin = append(allPathWithCoin, item)
+			}
+		}
+		SortPathByProfit(allPathWithCoin)
+		result = append(result, allPathWithCoin...)
+	}
+
+	return result
 }
